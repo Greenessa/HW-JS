8ebca65e74ea726390ce25e21f36f0881f086ad7
Margo <romanova184@yandex.ru>
2024-10-04 18:15:05 +0300

    fifth commit

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,34 +1,16 @@
 
-const dropdown = document.querySelector('.dropdown');
 const listAll = document.querySelector('.dropdown__list');
 const listItems = document.querySelectorAll('.dropdown__link');
 
 const valuePoint = document.querySelector('.dropdown__value');
 
-dropdown.addEventListener('click', function(event) {
-  // listAll.classList.toggle('dropdown__list_active');
-  listAll.classList.add('dropdown__list_active');
-  // event.stopPropagation();
+valuePoint.addEventListener('click', function(event) {
+  listAll.classList.toggle('dropdown__list_active');
 })
-
-
-
-// for (let item of listItems) {
-//     item.addEventListener('click', function(ev) {
-//     //   console.log(item.children[0].textContent);
-//       value.textContent = item.children[0].textContent;
-//       list.classList.remove('dropdown__list_active');
-//       })  
-//     }
 
 for (let i=0; i < listItems.length;i++) {
   listItems[i].addEventListener('click', function(e) {
-    console.log(e.target);
-    valuePoint.textContent=listItems[i].textContent;
-    console.log(valuePoint.textContent);
-    // listAll.classList.remove('dropdown__list_active'); 
+    e.preventDefault();
+    valuePoint.textContent=this.textContent;
   })
   }
-
-
- 
