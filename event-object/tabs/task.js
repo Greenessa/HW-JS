bf2b0a55f91b75b628ae72a17bcdea9e3f26a38d
Margo <romanova184@yandex.ru>
2024-10-06 19:14:08 +0300

    next commit

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,15 +1,12 @@
 let listTabs = Array.from(document.querySelectorAll('.tab'));
 let listTabsContent = Array.from(document.querySelectorAll('.tab__content'));
-document.addEventListener('click', (event) => {
-    // console.log(event.target);
-    iClick = listTabs.indexOf(event.target);
-    // console.log(iClick);
-    if (iClick>-1) {
-        let activeTab = document.querySelector('.tab_active');
-        let activeTabContent = document.querySelector('.tab__content_active');
-        activeTab.classList.remove('tab_active');
-        activeTabContent.classList.remove('tab__content_active');
-        listTabs[iClick].classList.add('tab_active');
-        listTabsContent[iClick].classList.add('tab__content_active')
-    } else return;
-});
+listTabs.forEach (function (tab, i) {
+    tab.addEventListener('click', (event) => {
+        const parent = event.target.closest('.tabs');
+        console.log(parent);
+        parent.querySelector('.tab_active').classList.remove('tab_active');
+        parent.querySelector('.tab__content_active').classList.remove('tab__content_active');
+        tab.classList.add('tab_active');
+        listTabsContent[i].classList.add('tab__content_active')
+    });
+})    
